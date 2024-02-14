18596a6b984a9970dbb6018e7a66acdb2745d745
sunnyjameel <sunnyjameel79@gmail.com>
2024-02-14 21:09:09 +0500

    First Program

--- Int.dart
+++ Int.dart
@@ -1,16 +1,15 @@
-
-void main (){
-
-List Number = [1,2,3,4,5,6,7,8,9];
-List even = [];
-List odd = [];
-for (var i=0;i<Number.length;i++){
-if(Number [i]%2 ==0){
-  even.add(Number[i]);
-} else{
-  odd.add(Number[i]);
-}
-}
-print(even);
-print(odd);
+void main() {
+  List Number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  List even = [];
+  List odd = [];
+  for (var i = 0; i < Number.length; i++) {
+    if (Number[i] % 2 == 0) {
+      even.add(Number[i]);
+    } else {
+      odd.add(Number[i]);
+    }
+  }
+  print("Hello World");
+  print("$even");
+  print(odd);
 }
